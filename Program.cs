bcec977eb09acf573ffff005b32a37c36aaf84eb
Serg <Serg.srav@mail.ru>
2023-05-26 21:09:18 +0500

    Домашнее задание 2

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿  //      Задача 1
+
+
+//Console.WriteLine("Введите трехзначное число");
+//int  num=Convert.ToInt32(Console.ReadLine());
+
+//int num1= num%100;
+//int num2=num1/10;
+
+//Console.WriteLine(num2);
+
+
+
+ //       Задача2
+
+
+ //Console.WriteLine("Введите  число");
+//int  num=Convert.ToInt32(Console.ReadLine());
+
+//int num1= num%100;
+//int num2=num1%10;
+
+//if (num<100)
+//{
+//    Console.WriteLine("Третьей цыфры нет");
+//}
+//else
+//{
+//    Console.WriteLine(num2);
+//}
+
+
+
+//      Задача 3
+
+//Console.WriteLine("Введите число");
+//int num=Convert.ToInt32(Console.ReadLine());
+
+//    if(num==6)
+//{
+//    Console.WriteLine("Да");
+//}
+//    else if(num==7)
+//{
+//    Console.WriteLine("Да");
+//}
+//    else if(num<6)
+ //   Console.WriteLine("Нет");
+
+ //   else if(num>7)
+ //   Console.WriteLine("Нет такого дня недели");
+    
+          
